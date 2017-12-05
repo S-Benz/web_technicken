90c1cafeec7a89e2bbcbf0338af82c8071f3be95
sbenz <sbenz.96@gmx.de>
2017-12-05 20:04:41 +0100

    Made header sticky

---
+++ docs/layout/assets/js/navigation.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener('scroll', stickyNav);
+    window.addEventListener('resize', stickyNav);
+
+    function stickyNav() {
+        var nav = document.querySelector('.nav__inner');
+        var contentText = document.querySelector('.content__text');
+        var content = document.querySelector('.content');
+        var sidebar = document.querySelector('.content__sidebar');
+        var headerHeight = document.querySelector('header').offsetHeight;
+        var navHeight = nav.offsetHeight;
+        var stickyTriggerHeight = headerHeight + navHeight;
+
+
+        if(window.pageYOffset >= stickyTriggerHeight) {
+            nav.style.position = 'fixed';
+            nav.style.top = 0;
+            nav.style.width = "100%";
+            sidebar.style.position = 'fixed';
+            contentText.style.marginLeft = 0;
+            if(window.outerWidth  < 768) {
+                sidebar.style.top = "15px";
+            } else {
+                sidebar.style.top = navHeight + "px";
+                contentText.style.marginLeft = "30%";
+                if(window.outerWidth >= 1200) {
+                    nav.style.width = "1200px";
+                    sidebar.style.width = (content.offsetWidth * 0.3) + "px";
+                }
+            }
+
+        }
+        if(window.pageYOffset < stickyTriggerHeight) {
+            nav.style.position = 'relative';
+            if(window.outerWidth  < 768) {
+                sidebar.style.position = 'absolute';
+                sidebar.style.top = "-55px";
+            } else {
+                sidebar.style.position = 'relative';
+                sidebar.style.top = 0;
+                contentText.style.marginLeft = 0;
+                nav.style.width = "100%";
+            }
+        }
+    }
+});
