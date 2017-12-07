a652eab81108389b282367846c2399ce328fae48
sbenz <sbenz.96@gmx.de>
2017-12-07 18:06:59 +0100

    Added comment to table sort

--- docs/layout/assets/js/pages/table/customer-table-sort.js
+++ docs/layout/assets/js/pages/table/customer-table-sort.js
@@ -1,29 +1,26 @@
 function sortTable() {
     var table = document.getElementById('customerTable');
     var rows = table.getElementsByTagName('tr');
-    var tbody = document.getElementById('tableBody');
-    var switchRows = true;
-    var tmpArray = [];
+    var tBody = document.getElementById('tableBody');
+    //Enable swapping
+    var swapping = true;
 
-    for(var j = 1; j < rows.length; j++) {
-        tmpArray.push([rows[j].getElementsByTagName('td')[1].innerHTML,j]);
-    }
-
-    tmpArray.sort();
-    console.log(tmpArray);
-
-    /*
-    while (switchRows) {
-        for (var i = 1; i < (rows.length - 1) ; i++) {
-            switchRows = false;
+    while (swapping) {
+        //Go through all table rows
+        for (var i = 1; i < (rows.length - 1); i++) {
+            swapping = false;
             var row1 = rows[i].getElementsByTagName('td')[1];
             var row2 = rows[i + 1].getElementsByTagName('td')[1];
 
-            if(row2.innerHTML.toLowerCase() < row1.innerHTML.toLowerCase()) {
-                tbody.insertBefore(rows[i+1], rows[i]);
-                switchRows = true;
+            /*
+               Check if two rows should be swapped, if they get swapped restart the iteration
+               Else continue iterating through all rows.
+             */
+            if (row2.innerHTML.toLowerCase() < row1.innerHTML.toLowerCase()) {
+                tBody.insertBefore(rows[i + 1], rows[i]);
+                swapping = true;
                 break;
             }
         }
-    }*/
+    }
 }
